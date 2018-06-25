450ab6cca477cd9bd4870b326370b28c98c627d8
A06438_P5.Training <A06438_P5.Training@P5-GL12T72.dir.svc.accenture.com>
2018-06-25 17:49:05 +0530

    commit55

--- src/test/java/example/org/seleniumtraining/BookTicket.java
+++ src/test/java/example/org/seleniumtraining/BookTicket.java
@@ -3,9 +3,13 @@ package example.org.seleniumtraining;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
@@ -21,10 +25,25 @@ public class BookTicket {
 	  LoginPageObject.uname.sendKeys(txtusername);
 	  LoginPageObject.password.sendKeys(txtpassword);
 	  LoginPageObject.Login_button.click();
+	  Assert.assertEquals("Find a Flight: Mercury Tours: ", driver.getTitle());
 	  /*driver.findElement(By.name("userName")).sendKeys("abc");
 	  driver.findElement(By.name("password")).sendKeys("abc");
 	  driver.findElement(By.name("login")).click();*/
 	 
+  }
+  @Test(priority=2)
+  public void findFlight()
+  {
+	Select p=new Select(driver.findElement(By.name("passCount"))); 
+	p.selectByValue("2");
+	Select d=new Select(driver.findElement(By.name("fromPort"))); 
+	d.selectByValue("London");
+	Select m=new Select(driver.findElement(By.name("fromMonth"))); 
+	m.selectByValue("1");
+	driver.findElement(By.name("servClass")).click();
+	driver.findElement(By.xpath("/html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[14]/td/input")).click();
+	
+	
   }
   
   @DataProvider(name="logindata")
